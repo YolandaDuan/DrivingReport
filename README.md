7b236a48ac4d1176ecf2b872dec5df56dc0fb9ad
yolanda <malmsduan@gmail.com>
2021-10-05 00:41:39 +0200

    generate driving report

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Requirements from [Springworks](https://github.com/Springworks/recruitment-waypoints-challenge). 
 2. Reference
 - [Convert JSON to Table](https://www.encodedna.com/javascript/practice-ground/default.htm?pg=convert_json_to_table_javascript).
+- [Calculate distance based on latitude and longitude](https://www.geodatasource.com/developers/javascript).
--- main.js
+++ main.js
@@ -4,61 +4,9 @@ const url = "https://raw.githubusercontent.com/Springworks/recruitment-waypoints
 r.open("GET", url, true);
 
 r.onreadystatechange = () => {
-    if (r.readyState !== 4 || r.status !== 200) return;
-    let wayPoints = JSON.parse(r.responseText);
-    let distSpeeding = 0, duraSpeeding = 0, totDistance= 0, totDuration = 0; 
-    for (i = 0; i < wayPoints.length; i++) {
-        const limit = 8.33;
-        let tsN = new Date(wayPoints[i].timestamp);
-        data.innerHTML += '<li>' + tsN.toLocaleString() + '</li>'
-        if (i > 0) {
-            let tsO = new Date(wayPoints[i-1].timestamp);
-            let interval = tsN.getSeconds() - tsO.getSeconds();
-            let latO = wayPoints[i-1].position.latitude;
-            let latN = wayPoints[i].position.latitude;
-            let lonO = wayPoints[i-1].position.longitude;
-            let lonN = wayPoints[i].position.longitude;
-            console.log(latO, latN, lonO, lonN);
-            let distance = (latO, latN, lonO, lonN) => {
-                if ((latN === latO) && (lonN === lonO)) {
-                    return 0;
-                } else {
-                    radLatO = Math.PI * latO/180;
-                    radLatN = Math.PI * latN/180;
-                    theta = lonN-lonO;
-                    radTheta = Math.PI * theta/180;
-                    let dist = Math.sin(radLatN) * Math.sin(radLatO) + Math.cos(radLatN) * Math.cos(radLatO) * Math.cos(radTheta);
-               
-                    if (dist > 1) {
-                        dist = 1;
-                    }
-                    dist = Math.acos(dist) * 180/Math.PI * 60 * 1.1515;
-                    // return distance in meters
-                    return dist * 1609.344;   
-                }
-            }
-            let distanceON = distance(latO, latN, lonO, lonN);
-            console.log("distance is: " + distanceON.toFixed(2) + " meters");
-            let speed = (distanceON/interval).toFixed(2);
-            console.log("speed is: " + speed + " meters/second");
-            if (speed > limit) {
-                distSpeeding += distanceON;
-                duraSpeeding += interval;
-                totDistance += distanceON;
-                totDuration += interval;
-            } else {
-                totDistance += distanceON;
-                totDuration += interval;
-            }
-        }      
-    }
-    console.log("Speedy distance equals to: " + distSpeeding.toFixed(2) + " meters.");
-    console.log("Speedy duration equals to: " + duraSpeeding + " seconds.");
-    console.log("Total driving distance equals to: " + totDistance.toFixed(2) + " meters.");
-    console.log("Total driving duration equals to: " + totDuration + " seconds.");
+    if (r.readyState !== 4 || r.status !== 200) return; 
 };
 r.send();
-
 
 let tableFromJson = () => {
     let wayPoints = JSON.parse(r.responseText);
@@ -78,22 +26,84 @@ let tableFromJson = () => {
 
     for (let i = 0; i < col.length; i++) {
         let th = document.createElement("th");
-        th.innerHTML = col[i];
-        tr.appendChild(th);
+        th.innerHTML = col[i].toUpperCase();
+        tr.append(th);
     }
 
     for (let i = 0; i < wayPoints.length; i++) {
         tr = table.insertRow(-1);
         for (let j = 0; j < col.length; j++) {
             let tabCell = tr.insertCell(-1);
-            if(j == 1) {
+            if(j === 0) {
+                let ts = new Date(wayPoints[i].timestamp);
+                tabCell.innerHTML = ts.toLocaleString();
+            } else if(j === 1) {
                 tabCell.innerHTML = wayPoints[i].position.latitude + ", " + wayPoints[1].position.longitude;
             } else {
                 tabCell.innerHTML = wayPoints[i][col[j]];
             }  
         }
     }
-    const divShowData = document.getElementById("showData");
-    divShowData.innerHTML = "";
-    divShowData.appendChild(table);
+
+    const divShowLog = document.getElementById("showLog");
+    divShowLog.innerHTML = "";
+    divShowLog.append(table);
 }
+
+let generateReport = () => {
+    let wayPoints = JSON.parse(r.responseText);
+    var distSpeeding = 0, duraSpeeding = 0, totDistance= 0, totDuration = 0; 
+    for (i = 0; i < wayPoints.length; i++) {
+        const limit = 8.33;
+        if (i > 0) {
+            let tsN = new Date(wayPoints[i].timestamp);
+            let tsO = new Date(wayPoints[i-1].timestamp);
+            let interval = tsN.getSeconds() - tsO.getSeconds();
+          
+            let calculateDist = (latO, latN, lonO, lonN) => {
+                if ((latN === latO) && (lonN === lonO)) {
+                    return 0;
+                } else {
+                    radLatO = Math.PI * latO/180;
+                    radLatN = Math.PI * latN/180;
+                    theta = lonN-lonO;
+                    radTheta = Math.PI * theta/180;
+                    let dist = Math.sin(radLatN) * Math.sin(radLatO) + Math.cos(radLatN) * Math.cos(radLatO) * Math.cos(radTheta);
+               
+                    if (dist > 1) {
+                        dist = 1;
+                    }
+                    dist = Math.acos(dist) * 180/Math.PI * 60 * 1.1515;
+                    // return distance in meters
+                    return dist * 1609.344;   
+                }
+            }
+
+            let latO = wayPoints[i-1].position.latitude;
+            let latN = wayPoints[i].position.latitude;
+            let lonO = wayPoints[i-1].position.longitude;
+            let lonN = wayPoints[i].position.longitude;
+
+            let distanceON = calculateDist(latO, latN, lonO, lonN);
+            console.log(`Distance between waypingt[${i}] and waypoint[${i+1}] is: ${distanceON.toFixed(2)} meters`);
+            let speed = (distanceON/interval).toFixed(2);
+            console.log("Speed is: " + speed + " meters/second");
+            if (speed > limit) {
+                distSpeeding += distanceON;
+                duraSpeeding += interval;
+                totDistance += distanceON;
+                totDuration += interval;
+            } else {
+                totDistance += distanceON;
+                totDuration += interval;
+            }
+        }      
+    }
+    const divShowReport = document.getElementById("showReport");
+    divShowReport.innerHTML = `<p> Speedy distance equals to: <strong> ${distSpeeding.toFixed(2)} </strong> meters.</p>
+                               <p> Speedy duration equals to: <strong> ${duraSpeeding} </strong> seconds.</p>
+                               <p> Total driving distance equals to: <strong> ${totDistance.toFixed(2)} </strong> meters.</p>
+                               <p> Total driving duration equals to: <strong> ${totDuration} </strong> seconds.</p>`;
+}
+
+
