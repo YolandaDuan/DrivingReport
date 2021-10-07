7e4747c96bf4de5dbb324a7aad5dc94039c5f84a
yolanda <malmsduan@gmail.com>
2021-10-07 13:20:13 +0200

    add index column to log table, show categorised waypoints at driving report

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-let data = document.getElementById('data');
-let r = new XMLHttpRequest();
 const url = "https://raw.githubusercontent.com/Springworks/recruitment-waypoints-challenge/master/waypoints.json";
+
+let r = new XMLHttpRequest();
 r.open("GET", url, true);
 
-r.onreadystatechange = () => {
-    if (r.readyState !== 4 || r.status !== 200) return; 
-};
 r.send();
 
-let tableFromJson = () => {
+tableFromJson = () => {
     let wayPoints = JSON.parse(r.responseText);
     console.log(wayPoints);
     let col = [];
@@ -22,35 +19,46 @@ let tableFromJson = () => {
     }
     let logTable = document.createElement("table");
 
-    let tr = logTable.insertRow(-1);
+    let tr = logTable.insertRow();
 
-    for (let i = 0; i < col.length; i++) {
+    for (let i = 0; i < col.length + 1; i++) {
         let th = document.createElement("th");
-        th.innerHTML = col[i].toUpperCase();
+        if (i === col.length) {
+            th.innerText = "No.";
+        } else {
+            th.innerHTML = col[i].toUpperCase();
+        }
         tr.append(th);
     }
 
     for (let i = 0; i < wayPoints.length; i++) {
-        tr = logTable.insertRow(-1);
+        let tr = logTable.insertRow();
         for (let j = 0; j < col.length; j++) {
-            let tabCell = tr.insertCell(-1);
+            let td = tr.insertCell();
             if(j === 0) {
                 let ts = new Date(wayPoints[i].timestamp);
-                tabCell.innerHTML = ts.toLocaleString();
+                td.innerHTML = ts.toLocaleString();
             } else if(j === 1) {
-                tabCell.innerHTML = wayPoints[i].position.latitude + ", " + wayPoints[1].position.longitude;
+                td.innerHTML = wayPoints[i].position.latitude + ", " + wayPoints[1].position.longitude;
             } else {
-                tabCell.innerHTML = wayPoints[i][col[j]];
+                td.innerHTML = wayPoints[i][col[j]];
             }  
         }
     }
 
-    const divShowLog = document.getElementById("showLog");
+    for(let i = 1; i < logTable.rows.length; i++) {
+        console.log(logTable.rows.length);
+        let index = document.createTextNode(i);
+        let indexT = document.createTextNode("No.");
+        logTable.rows[i].insertCell(logTable.rows[i].cells.length).append(index);   
+    }
+
+    let divShowLog = document.getElementById("showLog");
     divShowLog.innerHTML = "";
     divShowLog.append(logTable);
 }
 
-let calculateDist = (latO, latN, lonO, lonN) => {
+calculateDist = (latO, latN, lonO, lonN) => {
     if ((latN === latO) && (lonN === lonO)) {
         return 0;
     } else {
@@ -68,10 +76,12 @@ let calculateDist = (latO, latN, lonO, lonN) => {
         return dist * 1609.344;   
     }
 }
+// module.exports = calculateDist;
 
-let finalCalcu = (wayPoints) => {
+finalCalcu = (wayPoints) => {
     let distSpeeding = 0, duraSpeeding = 0, totDistance= 0, totDuration = 0; 
     const limit = 8.33;
+    let out = "";
     for (i = 1; i < wayPoints.length; i++) {
         let tsN = new Date(wayPoints[i].timestamp);
         let tsO = new Date(wayPoints[i-1].timestamp);
@@ -84,35 +94,49 @@ let finalCalcu = (wayPoints) => {
         const [latN, lonN] = coordinateN;
 
         let distanceON = calculateDist(latO, latN, lonO, lonN);
-        console.log(`Distance between waypoint[${i}] and waypoint[${i+1}] is: ${distanceON.toFixed(2)} meters`);
         let speed = (distanceON/interval).toFixed(2);
-        console.log("Speed is: " + speed + " meters/second");
+        out += `<h4>Waypoint ${i} and ${i+1}</h4> 
+                    <p> Distance: ${distanceON.toFixed(2)} meters,
+                        Speed: ${speed} meters/second,
+                        Duration: ${interval} seconds.
+                        <span id="overspeed">OVERSPEED</span>
+                    </p>`;
         if (speed > limit) {
             distSpeeding += distanceON;
             duraSpeeding += interval;
             totDistance += distanceON;
             totDuration += interval;
+
         } else {
             totDistance += distanceON;
             totDuration += interval;
+            document.getElementById("overspeed").style.display = "none";
         }     
     }
-   
+    console.log("out is: " + out);
+    let test = document.createElement("div");
+    test.innerHTML = out; 
+    let addTest = document.getElementById("test");
+    addTest.append(test);
+    console.log("test is: " + test);
+    console.log("addTest is: " + addTest);
     return [distSpeeding, duraSpeeding, totDistance, totDuration];
 }
 
-let generateReport = () => {
+generateReport = () => {
     let wayPoints = JSON.parse(r.responseText);
     
     const [distSpeeding, duraSpeeding, totDistance, totDuration] = finalCalcu(wayPoints);
 
-    const divShowReport = document.getElementById("showReport");
-    divShowReport.innerHTML = `<ol>
-                                <li> Speeding distance equals to: &nbsp; <strong>${distSpeeding.toFixed(2)}</strong> &nbsp; meters.</li>
-                                <li> Speeding duration equals to: &nbsp; <strong>${duraSpeeding}</strong> &nbsp; seconds.</li>
-                                <li> Total driving distance equals to: &nbsp;<strong>${totDistance.toFixed(2)}</strong> &nbsp; meters.</li>
-                                <li> Total driving duration equals to: &nbsp;<strong>${totDuration}</strong> &nbsp; seconds.</li>
-                               </ol>`;   
+    let divShowReport = document.getElementById("showReport");
+    divShowReport.innerHTML = `<h3>IN TOTAL</h3>
+                                <ol>
+                                    <li> Speeding distance equals to: &nbsp; <strong>${distSpeeding.toFixed(2)}</strong> &nbsp; meters.</li>
+                                    <li> Speeding duration equals to: &nbsp; <strong>${duraSpeeding}</strong> &nbsp; seconds.</li>
+                                    <li> Total driving distance equals to: &nbsp;<strong>${totDistance.toFixed(2)}</strong> &nbsp; meters.</li>
+                                    <li> Total driving duration equals to: &nbsp;<strong>${totDuration}</strong> &nbsp; seconds.</li>
+                                </ol>`;   
+    // divShowReport.append(test);
 
 }
 
